b41a9935bdf85c3dda37519512d47d2728cfd424
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-01-04 09:27:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-// var x = (3, 4, 1, 8);
-// console.log(x);
-// var x = 4;
-// var y = 3;
+var name = "Banti Kumar";
+var name = "Aman Burnwal";
 
-// console.log('The addition of 5 + 3 is : $[x + y}');
+let rool = 1;
+//let rool = 2; // throws error cause you can't redecleare
 
-
-// let num = 5;
-// console.log(num++);
-// console.log(num);
-// console.log(++num);
-
-// let newNum = 45;
-// newNum = newNum++;
-// const newNum2 = ++newNum 
-
-// console.log(newNum2);
+const a = 3;
+// a = 5;
+console.log(a);
