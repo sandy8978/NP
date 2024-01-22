d5af5e8141599a113e82ed706217fea7f218e72e
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-01-22 09:01:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-let n = parseInt(readline());
-	while(n--)
-   {
-      let arr = [];
-	arr = readline().split(" ");
-	var a = arr[0];
-	var b = arr[1];
-	var c = arr[2];
-      var l;
-// write your code here
-    arr = "45 23 54"
-	if((a > b && a < c) ||(a < b && a > c)){
-    	console.log(a);
-	}else if((b > a && b < c) || (b < a && b > c)){
-    	console.log(b)
-	}else{
-    	console.log(c);
-	}
+const jsonfile=require('jsonfile');
+const moment=require('moment');
+const FILE_PATH='./data.json';
+const DATE=moment().format();
+const data={
+    date:DATE
 }
+jsonfile.writeFile(FILE_PATH,data)
