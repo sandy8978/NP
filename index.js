3e2fb84d4feab61481e1f3a0d66f3d1dff241bc6
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-01-25 12:48:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const jsonfile=require('jsonfile');
-const moment=require('moment');
-const FILE_PATH='./data.json';
-const DATE=moment().format();
-const data={
-    date:DATE
-}
-jsonfile.writeFile(FILE_PATH,data)
+var name = "Banti Kumar";
+var name = "Aman Burnwal";
+
+let rool = 1;
+//let rool = 2; // throws error cause you can't redecleare
+
+const a = 3;
+// a = 5;
+console.log(a);
