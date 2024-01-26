f274b74d9d37abda7e85c2c16b6f3888baa30f3d
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-01-26 09:31:16 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,42 +1,7 @@
-class LinkedList {
-    constructor(data) {
-        this.head = {
-            value: data,
-            next: null,
-        };
-        this.tail = this.head;
-        this.size = 1;
-    }
+// var ame = "aman burnwal";
+// var bame = "Aman Burnwal"
+// console.log(ame);
+// console.log(bame);
 
-    // adding new value
-    add(data) {
-        let newNode = {
-            value: data,
-            next: null,
-        };
-
-        this.tail.next = newNode;
-        this.tail = newNode;
-        this.size++;
-
-    }
-    // travesing
-
-    traversing() {
-        let counter = 0;
-        let currentNode = this.head;
-        while (counter < this.size) {
-            console.log(currentNode);
-            currentNode = currentNode.next;
-            counter++
-        }
-    }
-}
-
-
-const list1 = new LinkedList(40);
-
-list1.add(43);
-list1.add(56);
-list1.traversing();
-// console.log(list1);
+let i = "Aman";
+console.log(i);
