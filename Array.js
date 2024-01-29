8a4c473094e596cb509723f3112e81c456e22488
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-01-29 10:51:43 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,13 @@
+let array=[1,2,3,4]
+let array1=[2,"sanddep",true];
+console.log(array)
+console.log(array1)
+array[0]="nag";
+console.log(array)
+let name="sandeep";
+let age=25;
+let ojj={
+    name:"san",
+    age:26,
+};
+console.log("name is "+ojj.name,age);
