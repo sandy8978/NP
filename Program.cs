fa0858ea074cd831fffdd01d02bdab434f5bc471
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-02-04 19:42:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CAArrays
+{
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = new int[] { 12, 43, 21, 65, 42, 22, 11, 4, 9, 98, 54 };
+            int size = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0)
+                {
+                    size++;
+                }
+            }
+            int[] br = new int[size];
+            int index = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (ar[i] % 2 == 0)
+                {
+                    br[index] = ar[i];
+                    index++;
+                }
+            }
+
+            foreach (var item in br)
+            {
+                Console.Write(item + " ");
+            }
+
+
+            //int[] br = new int[ar.Length];
+            //Array.Copy(ar, br, ar.Length);
+            //Array.Sort(br);
+            //Array.Reverse(br);
+            //foreach(var item in br)
+            //{
+            //    Console.Write(item+"  ");
+            //}
+        }
+    }
+}
+
---
+++ sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter your Name:");
+            //int name = Console.Read();    -- To Print ASCI VALUE -- //
+
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("My name is " + name);
+            Console.ReadKey();
+
+
+        }
+    }
+}
