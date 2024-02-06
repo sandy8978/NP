f2d27b0e3570366ec78323dc1a47332bdbfeeeeb
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-02-06 10:56:48 +0530

    Add files via upload

---
+++ sample18.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample18
+    {
+        int studentid;
+        string studentName;
+        string studentC_Name;
+        string studentSubject;
+        double studentMarks;
+
+        public void GetEmpData()
+        {
+            Console.WriteLine("Enter student Details:");
+            studentid = Convert.ToInt32(Console.ReadLine());
+            studentName = Console.ReadLine();
+            studentC_Name = Console.ReadLine();
+            studentSubject = Console.ReadLine();
+            studentMarks = Convert.ToDouble(Console.ReadLine());
+
+
+                }
+        public void DisplayEmpData()
+        {
+            Console.WriteLine($"StuId:{studentid},StuName:{studentName},StudentC_Name:{studentC_Name},StudentSubject:{studentSubject},StudentMarks:{studentMarks}");
+
+        }
+        public string Greet()
+        {
+            return "Welcome To C# Programing";
+        }
+        public static void Main()
+        {
+            sample18 obj = new sample18();
+            obj.Greet();
+            obj.GetEmpData();
+            obj.DisplayEmpData();
+            obj.Greet();
+
+        }
+    }
+  
+}
---
+++ sample19.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample19
+    {
+        public static void Main()
+        {
+            string[,] strArray = new string[2, 4] { { "C#.Net", "VB.Net", "J#.Net", "F#.Net" }, { "HTML", "CSS", "Javascript", "Bootstrap" } };
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.WriteLine(strArray[r, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
